6be7329cb869e1e4bbbfa4f00a46e6a74193c22a
rh-bjeon <108232840+rh-bjeon@users.noreply.github.com>
2025-04-25 09:57:35 +0900

    Update check-and-recreate-projects.sh

--- bootstrap/ic-user-projects/tools/check-and-recreate-projects.sh
+++ bootstrap/ic-user-projects/tools/check-and-recreate-projects.sh
@@ -11,7 +11,7 @@ WORKBENCH_NAME="my-workbench"
 WORKBENCH_IMAGE="ic-workbench:1.2"
 PIPELINE_ENGINE="Tekton"
 projects_without_running_pods=()
-BRANCH_NAME="kor-main-rhoai-2.13"
+BRANCH_NAME="kor-rhoai-2.13"
 
 for i in $(seq 1 $user_count);
 do
@@ -519,7 +519,7 @@ spec:
         args:
         - -ec
         - |-
-          pod_name=\$(oc get pods --selector=app=$WORKBENCH_NAME -o jsonpath='{.items[0].metadata.name}') && oc exec \$pod_name -- git clone https://github.com/rh-aiservices-bu/parasol-insurance && cd parasol-insurance && git checkout $BRANCH_NAME
+          pod_name=\$(oc get pods --selector=app=$WORKBENCH_NAME -o jsonpath='{.items[0].metadata.name}') && oc exec \$pod_name -- git clone https://github.com/rh-bj/parasol-insurance && cd parasol-insurance && git checkout $BRANCH_NAME
       restartPolicy: Never
 EOF
 
