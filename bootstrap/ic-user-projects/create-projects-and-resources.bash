5feeee945ebb03c6e04c74fd86bd8b441f7d6e34
rh-bjeon <108232840+rh-bjeon@users.noreply.github.com>
2025-04-25 09:59:45 +0900

    Update create-projects-and-resources.bash

--- bootstrap/ic-user-projects/create-projects-and-resources.bash
+++ bootstrap/ic-user-projects/create-projects-and-resources.bash
@@ -25,7 +25,7 @@ DASHBOARD_ROUTE=https://$(oc get route rhods-dashboard -n redhat-ods-application
 WORKBENCH_NAME="my-workbench"
 WORKBENCH_IMAGE="ic-workbench:1.2"
 PIPELINE_ENGINE="Argo"
-BRANCH_NAME="main-rhoai-2.13"
+BRANCH_NAME="kor-rhoai-2.13"
 
 for i in $(seq 1 $user_count);
 do
@@ -496,7 +496,7 @@ spec:
         args:
         - -ec
         - |-
-          pod_name=\$(oc get pods --selector=app=$WORKBENCH_NAME -o jsonpath='{.items[0].metadata.name}') && oc exec \$pod_name -- git clone https://github.com/rh-aiservices-bu/parasol-insurance && cd parasol-insurance && git checkout $BRANCH_NAME
+          pod_name=\$(oc get pods --selector=app=$WORKBENCH_NAME -o jsonpath='{.items[0].metadata.name}') && oc exec \$pod_name -- git clone https://github.com/rh-bj/parasol-insurance && cd parasol-insurance && git checkout $BRANCH_NAME
       restartPolicy: Never
 EOF
 
